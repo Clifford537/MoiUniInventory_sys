f98cd6be8218e2f483044ec47f40fbf952859c41
Clifford537 <cliffordmukosh@gmail.com>
2024-08-03 16:04:16 +0300

    updates

--- MoiInventory/MoiInventory/settings.py
+++ MoiInventory/MoiInventory/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%8knq7on+yfgo_1ysajcjs33!kfhurib^gmpzn=za&v9p06)6)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
